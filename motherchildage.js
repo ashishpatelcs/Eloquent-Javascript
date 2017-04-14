3b4a315dc299d381b494087a4c7bf02f5bdbb48b
Ashish Patel <ashish.tripleclicks@gmail.com>
2017-04-14 12:30:33 +0530

    Create motherchildage.js

---
+++ motherchildage.js
@@ -0,0 +1,35 @@
+/* Author: Ashish Patel
+ * Date: 14/04/2017
+ */
+ 
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+function agediff(person) {
+  var mother = byName[person.mother];
+  if (mother === null || mother === undefined) {
+    return 0;
+  }
+  else return (person.born - mother.born);
+}
+
+var agearray = [];
+ancestry.forEach(function(person) {
+  var agedifference = agediff(person);
+  if(agedifference !== 0) {
+  	agearray.push(agediff(person));
+	}
+});
+console.log(agearray);
+console.log(average(agearray));
+
+console.log(byName["Carolus Haverbeke"]);
+
+// → 31.2
