5d301a48948f3d9493ab9ff61d2430a8b86628ed
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-08-13 04:44:14 +0530

    Provided comment for code.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -10,10 +10,15 @@
  */
 
 for (var i = 1; i <= 100; i++) {
+ // print FizzBuzz if i is divisible by 3 and 5.
   if (i % 5 === 0 && i % 3 === 0)
     console.log("FizzBuzz");
+ 
+ // print Buzz if i is divisible by 5.
   else if (i % 5 === 0 && i % 3 !== 0)
     console.log("Buzz");
+ 
+ // print Fizz if i is divisible by 3.
   else if (i % 3 === 0)
     console.log("Fizz");
   else console.log(i);
